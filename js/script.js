6dbd4e5a2662b1b1db34b60bc8dde2e0a4bb71bf
Zhakyp Zhoomart uulu <zhakyp.zhoomartuulu.kk@gamil.com>
2021-11-15 22:39:45 +0600

    media

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ $(".carousel_card_inner").owlCarousel({
     autoplayHoverPause: true,
 });
 
+const hamburger = document.querySelector('.header .header_top .navbar .hamburger');
+const mobile_menu = document.querySelector('.header .header_top .navbar ul');
+const menu_item = document.querySelectorAll('.header .header_top .navbar ul li a');
+const header = document.querySelector('.header.containers');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',() =>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
+
+menu_item.forEach(item=>{
+    item.addEventListener('click',() =>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
